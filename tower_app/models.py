0d20f75a0cd570fc379e54daa8a057fc32503bfd
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2020-01-09 14:35:29 -0500

    Enemy logic - still needs work.

--- tower_app/models.py
+++ tower_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from random import randint
+from random import randint, sample, choice
+from time import sleep
 
 
 class Room(models.Model):
@@ -26,25 +27,78 @@ class Player(models.Model):
 class Enemy(models.Model):
 
     def random_string(self):
-        return str(randint(0, 5))
+        return str(randint(1, 21))
 
-    name = models.CharField(max_lenght=64)
+    enemy_list = ['Giant Scorpion Spider',
+                  'Reaper',
+                  'Ogre',
+                  'Troll',
+                  'Creeper',
+                  'Witch',
+                  'Red Dragon',
+                  'Stone Beast',
+                  'Warlock',
+                  'Goblin']
+
+    name = models.CharField(max_length=64)
     location = models.ForeignKey(Room,
                                  on_delete=models.CASCADE,
                                  blank=True,
                                  null=True)
-    enemy_type = models.CharField(max_length=64)
-    hp = models.CharField(default=random_string)
-    offense_strength = models.CharField(default=random_string)
+    # enemy_type = models.CharField(max_length=64)
+    HP = models.CharField(default=random_string)
+    strength = models.CharField(default=random_string)
 
-    def create_enemy(self):
-        pass
+    def spawn_enemies(self):
 
-    def strike(self):
-        pass
+        rooms = Room.objects.all()
+        room_1 = choice(rooms)
+        room_2 = choice(rooms)
+        enemy_1 = choice(self.enemy_list)
+        enemy_2 = choice(self.enemy_list)
 
-    def perish(self):
-        pass
+        spawn_enemy1 = Enemy(name=enemy_1)
+        spawn_enemy2 = Enemy(name=enemy_2)
+
+        spawn_enemy1.save()
+        spawn_enemy2.save()
+
+        spawn_enemy1.location = room_1.id
+        spawn_enemy2.location = room_2.id
+
+        spawn_enemy1.save()
+        spawn_enemy2.save()
+            # spawned_enemies = sample(self.enemy_list, 2)
+            # spawned_enemies[0] = choice(self.location)
+            # spawned_enemies[1] = choice(self.location)
+
+        #     return f'{self.name}, Beware of your enemies!, {self.location.room_name}'
+        # else:
+        #     return f'{self.name}, Are you afraid to fight?'
+
+    def enemy_strikes_player(self, player):
+        player_room = player.room
+        enemy_in_room = Enemy.objects.filter(location=player_room)
+        if len(enemy_in_room) > 0:
+            sleep(5)
+            player.HP = player.HP - enemy_in_room[0].strength
+            player.save()
+            return f'{player.name}, You have been hit and now your HP is {player.HP}'
+        if player.HP <= 0:
+            player.room = Room.objects.first().id
+            player.save()
+            return f'{player.name}, Oh no! You have been slain. Please try again from the beginning.'
+
+    def player_strikes_enemy(self, player, enemy):
+        player_room = player.room
+        enemy_in_room = Enemy.objects.filter(location=player_room)
+        if len(enemy_in_room) > 0:
+            enemy.HP = enemy.HP - enemy_in_room[0].strength
+            return f"{player.name}, You have hit {enemy.name} and now {enemy.name}'s is {enemy.HP}"
+        if player.HP <= 0:
+            player.room = Room.objects.first().id
+            player.save()
+            return f'{player.name}, Victory! You have slayed {enemy.name}!'
 
     def __str__(self):
         return f'{self.name} in {self.location}'
